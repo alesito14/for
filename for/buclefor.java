6de6648d9546c3c576255072fb2f5a4ed9cd826c
Alejandro Rivera <141943838+alesito14@users.noreply.github.com>
2024-10-14 13:01:58 -0600

    Add files via upload

---
+++ for/buclefor.java
@@ -0,0 +1,10 @@
+
+public class buclefor{
+
+    public static void main(String[] args){
+
+        for (int cont=0; cont<=10;cont++){
+            System.out.println("estoy dando vueltas con for  " + cont);
+        }
+    }
+}
